92c429623a4ca20f6b0b6279b7b2db505626177b
KepaAmigoTECHFriendly <58561058+KepaAmigoTECHFriendly@users.noreply.github.com>
2023-03-10 09:41:06 +0100

    Add files via upload

--- text_to_speech.R
+++ text_to_speech.R
@@ -38,7 +38,7 @@ ids_df_audio <- df_audio$data.id$id
 atributo_id_audio <- c()
 keys <- URLencode(c("id"))
 for(i in 1:nrow(df_audio)){
-  
+
   url <- paste("http://plataforma:9090/api/plugins/telemetry/DEVICE/",ids_df_audio[i],"/values/attributes/SERVER_SCOPE?keys=", keys,sep = "")
   peticion <- GET(url, add_headers("Content-Type"="application/json","Accept"="application/json","X-Authorization"=auth_thb))
   # Tratamiento datos. De raw a dataframe
@@ -62,7 +62,7 @@ ids_df_parada <- df_parada$data.id$id
 atributo_id_parada <- c()
 keys <- URLencode(c("id"))
 for(i in 1:nrow(df_parada)){
-  
+
   url <- paste("http://plataforma:9090/api/plugins/telemetry/ASSET/",ids_df_parada[i],"/values/attributes/SERVER_SCOPE?keys=", keys,sep = "")
   peticion <- GET(url, add_headers("Content-Type"="application/json","Accept"="application/json","X-Authorization"=auth_thb))
   # Tratamiento datos. De raw a dataframe
@@ -77,12 +77,12 @@ for(i in 1:nrow(df_parada)){
 keys <- URLencode(c("tiempo_llegada_linea_1,tiempo_llegada_linea_2,tiempo_llegada_linea_3,parada_destino_linea1,parada_destino_linea2,parada_destino_linea3"))
 
 while(1){
-  
-  
+
+
   # ------------------------------------------------------------------------------
   # PETICIÓN TOKENs THB
   # ------------------------------------------------------------------------------
-  
+
   cuerpo <- '{"username":"kepa@techfriendly.es","password":"kepatech"}'
   post <- httr::POST(url = "http://plataforma:9090/api/auth/login",
                      add_headers("Content-Type"="application/json","Accept"="application/json"),
@@ -90,47 +90,47 @@ while(1){
                      verify= FALSE,
                      encode = "json",verbose()
   )
-  
+
   resultado_peticion_token <- httr::content(post)
   auth_thb <- paste("Bearer",resultado_peticion_token$token)
-  
-  
+
+
   for(i in 1:nrow(df_audio)){
-    
+
     # 1) RECOGIDA DE ATRIBUTOS ACTIVO TIPO PARADA
     posicion_id <- match(atributo_id_audio[i],atributo_id_parada)
     id_activo <- ids_df_parada[posicion_id]
-    
-    
+
+
     url <- paste("http://plataforma:9090/api/plugins/telemetry/ASSET/",id_activo,"/values/attributes/SERVER_SCOPE?keys=", keys,sep = "")
     peticion <- GET(url, add_headers("Content-Type"="application/json","Accept"="application/json","X-Authorization"=auth_thb))
     # Tratamiento datos. De raw a dataframe
     df <- jsonlite::fromJSON(rawToChar(peticion$content))
     df <- as.data.frame(df)
-    
-    
+
+
     posicion_tiempos <- grep("tiempo",df$key)
     posicion_destinos <- grep("destino",df$key)
-    
-    
+
+
     # 2) GENERACIÓN TEXTO
-    
+
     texto <- ""
     for(j in 1:length(posicion_destinos)){
       numero_linea <- as.numeric(gsub(".*?([0-9]+).*", "\\1", df$key[posicion_destinos[j]]))
       parada_destino <- df$value[posicion_destinos[j]]
       tiempo <- df$value[posicion_tiempos[j]]
-      
-      texto <- paste(texto, "Línea ",numero_linea," ",parada_destino," ", tiempo, ". ", sep = "")
+
+      texto <- paste(texto, "Autobús de Línea ",numero_linea," con destino ",parada_destino," llegará en ", tiempo, ". ", sep = "")
     }
-    
+
     texto <- gsub("\\<c3><b3>","ó",texto)
     texto <- gsub("<ó","ó",texto)
-    
-    
-    
+
+
+
     # 3) ESCRITURA EN ATRIBUTO DE DISPOSITIVO marquesina_audio
-    
+
     url <- paste("http://plataforma:9090/api/plugins/telemetry/DEVICE/", ids_df_audio[i], "/SHARED_SCOPE",sep = "")
     json_envio_plataforma <- paste('{"text2speech":"', texto,'"',
                                    '}',sep = "")
@@ -140,10 +140,10 @@ while(1){
                        verify= FALSE,
                        encode = "json",verbose()
     )
-    
+
   }
-  
+
   Sys.sleep(60)
-  
+
 }
 
